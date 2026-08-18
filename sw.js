4354e6dd9581c7f03701b9c2b42073a15cefefca
douglasneres106-ui <douglasneres106@gmail.com>
2026-08-18 19:38:28 -0300

    chore: refresh PWA cache for FITCOACH AI Pro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitcoach-v23'
+const CACHE_NAME = 'fitcoach-v24'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png']
 
 self.addEventListener('install', event => {
